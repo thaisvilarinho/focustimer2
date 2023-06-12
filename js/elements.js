95a97c981bae536f820959ba501958a19454617b
Thaís Aparecida Vilarinho de Jesus <thais.apvilarinho@hotmail.com>
2023-06-12 20:25:34 -0300

    Create elements.js

---
+++ js/elements.js
@@ -0,0 +1,13 @@
+const buttonPlay = document.querySelector(".play");
+const buttonPause = document.querySelector(".pause");
+const buttonStop = document.querySelector(".stop");
+const minutesDisplay = document.querySelector(".minutes");
+const secondsDisplay = document.querySelector(".seconds");
+
+export {
+  buttonPlay,
+  buttonPause,
+  buttonStop,
+  minutesDisplay,
+  secondsDisplay
+}
